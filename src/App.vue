b1ca0cb98c2f4e19900a896ea57669e6d0a9186f
yanJyli <85889195+yanJyli@users.noreply.github.com>
2022-07-27 22:21:40 +0300

    second - fetch + select

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="app">
-    <post-form @create="createPost"></post-form>
-    <post-list :posts="posts"></post-list>
+    <h1>Страница с постами</h1>
+    <div class="app__btns">
+      <my-button @click="showDialog">Создать пост</my-button>
+      <my-select v-model="selectedSort" :options="sortOptions"></my-select>
+    </div>
+
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost"></post-form>
+    </my-dialog>
+
+    <post-list
+      :posts="sortedPosts"
+      @remove="removePost"
+      v-if="!isPostLoading"
+    ></post-list>
+    <div v-else>Идет загрузка постов...</div>
   </div>
 </template>
 
 <script>
 import PostForm from "./components/PostForm.vue";
 import PostList from "./components/PostList.vue";
+import axios from "axios";
 
 export default {
   components: {
@@ -17,16 +32,50 @@ export default {
 
   data() {
     return {
-      posts: [
-        { id: 1, title: "JS 1", body: "Описание поста" },
-        { id: 2, title: "JS 2", body: "Описание поста" },
-        { id: 3, title: "JS 3", body: "Описание поста" },
+      posts: [],
+      dialogVisible: false,
+      isPostLoading: false,
+      selectedSort: "",
+      sortOptions: [
+        { value: "title", name: "По название" },
+        { value: "body", name: "По описанию" },
       ],
     };
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+      this.posts = this.posts.filter((p) => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    async fetchPost() {
+      try {
+        this.isPostLoading = true;
+        setTimeout(async () => {
+          const response = await axios.get(
+            "https://jsonplaceholder.typicode.com/posts?_limit=10"
+          );
+          this.posts = response.data;
+          this.isPostLoading = false;
+        }, 1000);
+      } catch (e) {
+        alert("Ошибка");
+      }
+    },
+  },
+  mounted() {
+    this.fetchPost();
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) =>
+        post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+      );
     },
   },
 };
@@ -40,5 +89,10 @@ export default {
 }
 .app {
   padding: 20px;
+}
+.app__btns {
+  display: flex;
+  justify-content: space-between;
+  margin: 10px;
 }
 </style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
+  <div v-if="posts.length > 0">
     <h3>Список постов</h3>
-    <post-item v-for="post in posts" :post="post" :key="post.id"></post-item>
+    <post-item
+      v-for="post in posts"
+      :post="post"
+      :key="post.id"
+      @remove="$emit('remove', post)"
+    ></post-item>
   </div>
+  <h2 v-else style="color:red">
+    Список постов пуст
+  </h2>
 </template>
 
 <script>
